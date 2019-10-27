8c241fe7c7719fa3f418a6c4925585c7207b07b9
saulo <saulosouza@outlook.com>
2019-10-27 11:36:48 -0300

    Terceiro Commit

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php
+$adicao = 2 + 2;
+$subtracao = 2 - 2;
+$multiplicada = 2 * 2;
+$divisao = 2 / 2;
+?>
+
+<!DOCtype html>
+<head>
+<title>Primeira Página de PHP</title>
+<meta charste="UTF-8">
+<body>
+    <h1>Meu primeiro arqivo de PHP</h1>
+    <p>2 + 2<?=$adicao?></p>
+    <p>2 - 2<?=$subtracao?></p>
+    <p>2 * 2<?=$multiplicada?></p>
+    <p>2 / 2<?=$divisao?></p>
+<body>
+</head>
+</html>
